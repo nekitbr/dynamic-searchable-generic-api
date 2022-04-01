33128940472b464e758aabfad03b8bfe6ee32b3a
Jose Dall Asta <nekitbr3131@gmail.com>
2022-04-01 02:18:45 -0300

    try to fix heroku port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 var express = require('express')
 var app = express()
 var host = process.env.URL_LISTENER
-var port = process.env.PORT_LISTENER
+var port = process.env.PORT
 
 app.use(express.static('public'))
 
